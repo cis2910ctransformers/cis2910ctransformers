a6669666ba92ef03e58c3a06715885cb2b9129a9
jgar198031 <59901009+jgar198031@users.noreply.github.com>
2020-02-25 18:40:53 -0500

    Add files via upload

---
+++ pages/years.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Museum | History</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+  <link rel="stylesheet" href="../styles/styles.css" />
+  <link rel="stylesheet" href="../styles/style-history.css" />
+</head>
+<body>
+<div id="topscroll"></div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img src="./../images/ai.png" height="50" width="50" alt="AI logo">
+    <a class="navbar-brand" href="./../index.html">AI Museum</a>
+    <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="./../index.html"rel="noopener noreferrer">Home <i class="fas fa-home"></i></a>
+            <a class="nav-item nav-link active" href="history.html"rel="noopener noreferrer">History <i class="fas fa-landmark"></i></a>
+            <a class="nav-item nav-link" href="machine-learning.htm" lrel="noopener noreferrer">Machine Learning <i class="fas fa-robot"></i></a>
+            <a class="nav-item nav-link" href="big-data.html" rel="noopener noreferrer">BigData <i class="fas fa-database"></i><span class="sr-only">(current)</span></a>
+
+        </div>
+    </div>
+</nav>
+<header></header>
+<main>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h1>1961</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img src="../images/1961.jpg" width="550px" height="400px" alt="placeholder+image">
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>The first industrial robot, Unimate, starts working on an assembly line in a General Motors plant in New Jersey. It was invented by George Devol in the 1950s using his original patent filed in 1954 and granted in 1961.</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h1>1962</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>The first industrial robot, Unimate, starts working on an assembly line in a General Motors plant in New Jersey. It was invented by George Devol in the 1950s using his original patent filed in 1954 and granted in 1961.</p>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img src="../images/1961.jpg" width="550px" height="400px" alt="placeholder+image">
+            
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h1>1963</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img src="../images/1961.jpg" width="550px" height="400px" alt="placeholder+image">
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>The first industrial robot, Unimate, starts working on an assembly line in a General Motors plant in New Jersey. It was invented by George Devol in the 1950s using his original patent filed in 1954 and granted in 1961.</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h1>1964</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+           <p>The first industrial robot, Unimate, starts working on an assembly line in a General Motors plant in New Jersey. It was invented by George Devol in the 1950s using his original patent filed in 1954 and granted in 1961.</p>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img src="../images/1961.jpg" width="550px" height="400px" alt="placeholder+image">
+            
+        </div>
+    </div>
+</div>
+</main>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h1>1965</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img src="../images/1961.jpg" width="550px" height="400px" alt="placeholder+image">
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>The first industrial robot, Unimate, starts working on an assembly line in a General Motors plant in New Jersey. It was invented by George Devol in the 1950s using his original patent filed in 1954 and granted in 1961.</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h1>1966</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <img src="../images/1961.jpg" width="550px" height="400px" alt="placeholder+image">
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>The first industrial robot, Unimate, starts working on an assembly line in a General Motors plant in New Jersey. It was invented by George Devol in the 1950s using his original patent filed in 1954 and granted in 1961.</p>
+        </div>
+    </div>
+</div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <footer>
+    <div class="container-fluid">
+      <a href="https://github.com/cis2910ctransformers/cis2910ctransformers/graphs/contributors">Contributors on Github
+        <i class="fab fa-github"></i></a>
+      <div id="copyright">
+        &copy;<small>Transformers 2020</small>
+      </div>
+    </div>
+  </footer>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  
+  <script src="../js/particles.js"></script>
+  <script src="../js/history.js"></script>
+<a id="scrollTop" title="Back to top" href="#"></a>
+</body>
+</html>
